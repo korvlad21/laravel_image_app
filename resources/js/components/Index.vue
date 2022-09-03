93e5be89f7c092e8d529c232b9aff95d865afeb7
korvlad21 <vladek2000@inbox.ru>
2022-09-03 13:32:36 +0300

    2 урок

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,26 +1,44 @@
 <template>
     <div class="w-25">
-        <div ref="dropzone" class="btn d-block p-5 bg-dark text-center text-light">
+        <input v-model="title" type="text" class="mb-3 form-control" placeholder="title">
+        <div ref="dropzone" class="mb-3 btn d-block p-5 bg-dark text-center text-light">
             Upload
         </div>
+        <input @click.prevent="store" type="submit" class="btn btn-primary" value="add">
     </div>
 </template>
 
 <script>
+import { file } from '@babel/types'
 import Dropzone from 'dropzone'
+
 export default {
     name: "Index",
     data(){
         return{
-            dropzone: null
+            dropzone: null,
+            title: null
         }
     },
 
     mounted() {
         this.dropzone = new Dropzone(this.$refs.dropzone, {
-            url: "sdfsdfsd",
-            clickable: true
+            url: '/api/posts',
+            autoProcessQueue: true
+
         })
+    },
+
+    methods:{
+        store(){
+            console.log(this.dropzone.getAcceptedFiles())
+            const images = new FormData()
+            const files = this.dropzone.getAcceptedFiles()
+            files.forEach(file => {
+                images.append('images[]', file)
+            })
+            axios.post('/api/posts', images)
+        }
     }
 }
 
